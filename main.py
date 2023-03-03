d3d92cf56669a13375ab97c75e1069e3b6254d9f
mndnet <nyberg@mndesign.net>
2023-03-03 16:22:37 +0100

    Added the option to have multiple discord roles

--- main.py
+++ main.py
@@ -27,11 +27,9 @@ async def on_message( message ):
     PatreonRole = []
     api_client = patreon.API(os.getenv("PATREON_TOKEN"))
 
-    #Get campaign ID
     campaign_response = api_client.fetch_campaign()
     campaign_id = campaign_response.data()[0].id()
 
-    #Get active roles on Patreon
     for rewardRole in campaign_response.data()[0].relationship('rewards'):
         if rewardRole.attribute('title') != None:
             PatreonRole.append(rewardRole.attribute('title'))
@@ -57,7 +55,6 @@ async def on_message( message ):
         declined = pledge.attribute('declined_since')
         reward_tier = 0
 
-        #Check if they have any tiers
         if pledge.relationships()['reward']['data']:
             reward_tier = pledge.relationship('reward').attribute('amount_cents')
 
@@ -66,48 +63,44 @@ async def on_message( message ):
             'total_historical_amount_cents': pledge.attribute('total_historical_amount_cents'),
             'patron_status': pledge.relationship('reward').attribute('status'),
             'tier': pledge.relationship('reward').attribute('title'),
-            'discord_role': pledge.relationship('reward').attribute('discord_role_ids')[0],
+            'discord_role': pledge.relationship('reward').attribute('discord_role_ids'),
             'discord_id': pledge.relationship('patron').attribute('social_connections')['discord']['user_id']
             })                
             
         for pledge in pledges_info:
-            has_role = 0    
-            #Give role to user
             member = message.guild.get_member(int(pledge['discord_id']))
             
-            if declined or reward_tier < 100:    
-                #Check if user has the role
+            if declined or reward_tier < 100:   
                 for singleRole in member.roles:
-                    if singleRole.id == int(pledge['discord_role']):
-                        role = message.guild.get_role(int(pledge['discord_role']))
-                        print(f"{now} - Role {role.name} was removed from {member.name}")
-                        await member.remove_roles(role)
+                    for singlePledgeRole in pledge['discord_role']:
+                        if singleRole.id == int(singlePledgeRole):
+                            role = message.guild.get_role(int(pledge['discord_role']))
+                            print(f"{now} - Role {role.name} was removed from {member.name}")
+                            await member.remove_roles(role)
             else:
-                #Check if user has the role
-                for singleRole in member.roles:
-                    if singleRole.id == int(pledge['discord_role']):
-                        has_role = 1
-                
-                if has_role == 0:
-                    role = message.guild.get_role(int(pledge['discord_role']))
-                    print(f"{now} - Role {role.name} was assigned to {member.name}")
-                    await member.add_roles(role)
+                for singlePledgeRole in pledge['discord_role']:
+                    role = message.guild.get_role(int(singlePledgeRole))
+                    if not role in member.roles:
+                        role = message.guild.get_role(int(singlePledgeRole))
+                        print(f"{now} - Role {role.name} was assigned to {member.name}")
+                        await member.add_roles(role)
 
     for singlePatreonRole in PatreonRole:
         role = discord.utils.find(
         lambda r: r.name == singlePatreonRole, message.guild.roles)
         
         for user in message.guild.members:
-            if role in user.roles: #all users with that role is found
+            if role in user.roles:
                 foundUser = 0
                 if not pledges_info:
                     print(f"{now} - Role {role.name} was removed from {user.name}")
                     await user.remove_roles(role)
 
-                for pledgeID in pledges_info: #get all roles from patreon
+                for pledgeID in pledges_info: 
                     if int(user.id) == int(pledgeID['discord_id']):
-                        if int(role.id) == int(pledgeID['discord_role']):
-                            foundUser = 1
+                        for singlePledgeRole in pledgeID['discord_role']:
+                            if int(role.id) == int(singlePledgeRole):
+                                foundUser = 1
 
                     if foundUser == 0:
                         print(f"{now} - Role {role.name} was removed from {user.name}")
